1304c5cff8ae284c27d9ab9433acd9c13d784c11
Igor Mineev <igron99@mail.ru>
2024-02-08 21:52:40 +0300

    Add ref-output

--- solution.c
+++ solution.c
@@ -4,9 +4,9 @@
 void kprintf(const char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
-    ...
+
     // fetch next argument
-    unsigned arg = va_arg(ap, unsigned);
-    ...
+    // unsigned arg = va_arg(ap, unsigned);
+
     va_end(ap);
 }
